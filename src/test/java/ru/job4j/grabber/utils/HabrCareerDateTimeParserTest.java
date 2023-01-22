f5e5c8a37bf9f117c00a5c9d40a6e52bbdfb0fd9
igor-grigoriev <grigoriev.i.v@mail.ru>
2023-01-22 09:56:45 +0300

    2.1. Преобразование даты [#289476] fix

--- src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
+++ src/test/java/ru/job4j/grabber/utils/HabrCareerDateTimeParserTest.java
@@ -8,7 +8,8 @@ class HabrCareerDateTimeParserTest {
     @Test
     public void testParser() {
         String date = "2023-01-01T01:01:01+03:00";
+        String expected = "2023-01-01T01:01:01";
         LocalDateTime result = new HabrCareerDateTimeParser().parse(date);
-        assertThat(date).contains(result.toString());
+        assertThat(result.toString()).isEqualTo(expected);
     }
 }
